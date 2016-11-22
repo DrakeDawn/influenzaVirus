a30ca7840d9cfe15d32cdfadfa1d5d707e3ee7e2
Cynric Fu <a454252753s@gmail.com>
2016-11-22 22:26:01 +0800

    Update counting.py

--- parsing/counting.py
+++ parsing/counting.py
@@ -1,17 +1,17 @@
 import csv
 
-infile1 = file('shuffle_for_CNN.csv', 'rb')
+infile1 = file('shuffle_CNN_validation.csv', 'rb')
 reader1 = csv.reader(infile1)
 
-infile2 = file('shuffle_for_CNN.csv', 'rb')
+infile2 = file('shuffle_CNN_training.csv', 'rb')
 reader2 = csv.reader(infile2)
 
 count = [0]*198
 
 for label, sequence in reader1:
-	count[label] += 1
+	count[int(label)] += 1
 for label, sequence in reader2:
-	count[label] += 1
+	count[int(label)] += 1
 
 index = sorted(range(len(count)), key = lambda k: count[k])
 
