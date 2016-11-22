b7c5715c2b61200bcfaacd171050c00327f822b0
Cynric Fu <a454252753s@gmail.com>
2016-11-22 22:18:30 +0800

    Add files via upload

---
+++ parsing/counting.py
@@ -0,0 +1,19 @@
+import csv
+
+infile1 = file('shuffle_for_CNN.csv', 'rb')
+reader1 = csv.reader(infile1)
+
+infile2 = file('shuffle_for_CNN.csv', 'rb')
+reader2 = csv.reader(infile2)
+
+count = [0]*198
+
+for label, sequence in reader1:
+	count[label] += 1
+for label, sequence in reader2:
+	count[label] += 1
+
+index = sorted(range(len(count)), key = lambda k: count[k])
+
+for n in index:
+	print('Influenza {}:'.format(n), count[n])
