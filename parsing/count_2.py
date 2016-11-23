0a86b6949681ed0e837e14d87c11ab7b19e5163b
Cynric Fu <a454252753s@gmail.com>
2016-11-23 09:39:09 +0800

    Create count_2.py

---
+++ parsing/count_2.py
@@ -0,0 +1,20 @@
+import csv
+
+infile1 = file('shuffle_CNN_validation.csv', 'rb')
+reader1 = csv.reader(infile1)
+
+infile2 = file('shuffle_CNN_training.csv', 'rb')
+reader2 = csv.reader(infile2)
+
+count = [0]*198
+
+for label, sequence in reader1:
+	count[int(label)] += 1
+for label, sequence in reader2:
+	count[int(label)] += 1
+
+index = sorted(range(len(count)), key = lambda k: count[k], reverse = True)
+
+for n in range(198):
+	if count(index(n)) > 10:
+		print '{},{}'.format(index(n), n)
