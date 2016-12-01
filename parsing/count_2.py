318a0cbaf232a1f63db306dd2e527aa1124c0aaa
Cynric Fu <a454252753s@gmail.com>
2016-12-01 18:53:51 +0800

    Update count_2.py

--- parsing/count_2.py
+++ parsing/count_2.py
@@ -1,10 +1,15 @@
 import csv
 
+threshold = 10
+
 infile1 = file('shuffle_CNN_validation.csv', 'rb')
 reader1 = csv.reader(infile1)
 
 infile2 = file('shuffle_CNN_training.csv', 'rb')
 reader2 = csv.reader(infile2)
+
+outfile = file('renaming.csv', 'wb')
+writer = csv.writer(outfile)
 
 count = [0]*198
 
@@ -16,5 +21,5 @@ for label, sequence in reader2:
 index = sorted(range(len(count)), key = lambda k: count[k], reverse = True)
 
 for n in range(198):
-	if count[inde[n]] > 10:
-		print '{},{}'.format(index[n], n)
+	if count[index[n]] > threshold:
+		writer.writerow(['{}'.format(index[n]), '{}'.format(n)])
